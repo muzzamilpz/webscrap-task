d770238ca1ae78648a8bac2195d2ba4c1a46d716
muzzamilpz <68339671+muzzamilpz@users.noreply.github.com>
2025-04-03 21:21:28 +0530

    Create constants.ts

---
+++ utils/constants.ts
@@ -0,0 +1,8 @@
+
+export const loginVerifyUrl = 'https://www.amazon.in/?ref_=nav_signin';
+
+export const fetchProductUrl = 'https://www.amazon.in/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2F%3Fref_%3Dnav_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=inflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+
+export const fetchOrderHistory = 'https://www.amazon.in/gp/your-account/order-history';
+
+export const fetchSearchHistory = "https://www.amazon.in/gp/legacy/order-history?opt=ab&search=";
